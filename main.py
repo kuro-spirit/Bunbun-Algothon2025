0ca837f1150e045d937f3dd02e29986d69ff9571
Bernice Seto <setotl2412@gmail.com>
2025-06-30 15:05:23 +1000

    edited getMyPosition method

--- main.py
+++ main.py
@@ -7,16 +7,50 @@ import numpy as np
 
 nInst = 50
 currentPos = np.zeros(nInst)
-
+prev_signal = np.zeros(nInst)
 
 def getMyPosition(prcSoFar):
-    global currentPos
+    global currentPos, prev_signal
     (nins, nt) = prcSoFar.shape
-    if (nt < 2):
+    if nt < 51:
         return np.zeros(nins)
-    lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])
-    lNorm = np.sqrt(lastRet.dot(lastRet))
-    lastRet /= lNorm
-    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:, -1]])
-    currentPos = np.array([int(x) for x in currentPos+rpos])
+
+    returns = np.log(prcSoFar[:, 1:] / prcSoFar[:, :-1])
+
+    short_window = 5
+    long_window = 50
+
+    sma_short = np.mean(prcSoFar[:, -short_window:], axis=1)
+    sma_long = np.mean(prcSoFar[:, -long_window:], axis=1)
+
+    trend = sma_long - sma_short
+    trend_signal = np.sign(trend)
+
+    vol_long = np.std(returns[:, -long_window:], axis=1)
+    vol_short = np.std(returns[:, -short_window:], axis=1)
+
+    vol_ratio = vol_short / (vol_long + 1e-8)
+    regime = vol_ratio > 1.5  # tighter filter
+
+    raw_signal = trend_signal * regime
+    raw_signal = np.where(np.abs(raw_signal) > 0.1, raw_signal, 0.0)
+
+    # Smooth signal
+    smoothed_signal = 0.7 * prev_signal + 0.3 * raw_signal
+    prev_signal = smoothed_signal
+
+    norm = np.sqrt(np.sum(smoothed_signal ** 2))
+    if norm > 0:
+        signal = smoothed_signal / norm
+    else:
+        signal = smoothed_signal
+
+    dollar_budget_per_instrument = 800  # slightly lower size
+
+    newPos = dollar_budget_per_instrument * signal / prcSoFar[:, -1]
+    newPos = np.array([int(x) for x in newPos])
+
+    # Reset position daily to reduce accumulation swings
+    currentPos = newPos
+
     return currentPos
